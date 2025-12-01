6de5bffe84ebee033ff43707bf576ee67bb9f3a2
김채령 <143330718+riela77@users.noreply.github.com>
2025-12-01 23:31:30 +0900

    #43 꺄륵 된당
    서버에 불러오는 값을 리스트 형태로 바꾸니 로딩됨.

--- BreezeRead/extension/src/content.js
+++ BreezeRead/extension/src/content.js
@@ -621,7 +621,7 @@ async function getFilter(currentArticleUrl) {
             console.log("✅ userFilter 존재:", userFilter);
             
             const gender = userFilter.gender;
-            const agesList = [userFilter.age];
+            const agesList = userFilter.agesList;
             
             console.log(`API에 전송: Gender: ${gender}, Age: ${agesList[0]}`);
             
@@ -656,7 +656,7 @@ async function recommend() {
     try {
         const finalRecommendData = await getFilter(articleUrl);
         console.log("최종 요약 데이터:", finalRecommendData);
-
+        
         // TODO: finalRecommendData 사용하여 사용자에게 결과를 보여주는 로직을 구현합니다.
 
     } catch (error) {
@@ -664,10 +664,9 @@ async function recommend() {
     }
 }
 
+
+
   recommend(); // 이 함수를 호출해야 실행됩니다.
-  // =========================================================================
-  // ⭐️ 실행 시작
-  // =========================================================================
   loadReadTimeAtInitialized();
   setupBookmarkSystem();
 })();
